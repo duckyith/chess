bf41af251bd0d7dffbf85b2f18397fbb74f21a0f
duckyith <bradyith@byu.edu>
2026-01-15 14:49:42 -0700

    ToStrings Overrided for Position and Move

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Represents a single chess piece
@@ -51,6 +52,10 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        ChessPiece piece = board.getPiece(myPosition);
+        if (piece.getPieceType() == PieceType.BISHOP) {
+            return List.of(new ChessMove(myPosition, new ChessPosition(1,8), null));
+        }
+        return List.of();
     }
 }
