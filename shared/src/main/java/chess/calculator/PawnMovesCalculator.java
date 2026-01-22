21e1dbdf720f11c1a24e5a47e1c1e60ff02b66d7
duckyith <bradyith@byu.edu>
2026-01-21 21:37:17 -0700

    Tried to do pawn, way harder than I thought. Quit

--- shared/src/main/java/chess/calculator/PawnMovesCalculator.java
+++ shared/src/main/java/chess/calculator/PawnMovesCalculator.java
@@ -1,8 +1,6 @@
 package chess.calculator;
 
-import chess.ChessBoard;
-import chess.ChessMove;
-import chess.ChessPosition;
+import chess.*;
 
 import java.util.ArrayList;
 
@@ -18,14 +16,35 @@ public class PawnMovesCalculator {
 
     public ArrayList<ChessMove> calc(){
         ArrayList<ChessMove> options = new ArrayList<ChessMove>();
-        options.addAll(new ValidCalc(board, position, 1, 1, true).calc(position));
-        options.addAll(new ValidCalc(board, position, -1, 1, true).calc(position));
-        options.addAll(new ValidCalc(board, position, 1, -1, true).calc(position));
-        options.addAll(new ValidCalc(board, position, -1, -1, true).calc(position));
-        options.addAll(new ValidCalc(board, position, 1, 0, true).calc(position));
-        options.addAll(new ValidCalc(board, position, 0, 1, true).calc(position));
-        options.addAll(new ValidCalc(board, position, -1, 0, true).calc(position));
-        options.addAll(new ValidCalc(board, position, 0, -1, true).calc(position));
+        if (board.getPiece(position).getTeamColor() == ChessGame.TeamColor.WHITE) {
+            options.addAll(new ValidCalc(board, position, 1, 0, false).calc(position));
+            if (position.getRow() == 2 && !options.isEmpty()){
+                options.addAll(new ValidCalc(board, position, 2, 0, false).calc(position));
+            }
+            if (position.getRow() == 7 && !options.isEmpty()){
+                ChessMove fake = options.getFirst();
+                options.clear();
+                ChessPosition target = new ChessPosition(position.getRow()+1,position.getColumn());
+                options.add(new ChessMove(position,target, ChessPiece.PieceType.BISHOP));
+                options.add(new ChessMove(position,target, ChessPiece.PieceType.ROOK));
+                options.add(new ChessMove(position,target, ChessPiece.PieceType.QUEEN));
+                options.add(new ChessMove(position,target, ChessPiece.PieceType.KNIGHT));
+            }
+        } else {
+            options.addAll(new ValidCalc(board, position, -1, 0, false).calc(position));
+            if (position.getRow() == 7 && !options.isEmpty()){
+                options.addAll(new ValidCalc(board, position, -2, 0, false).calc(position));
+            }
+            if (position.getRow() == 2 && !options.isEmpty()){
+                ChessMove fake = options.getFirst();
+                options.clear();
+                ChessPosition target = new ChessPosition(position.getRow()-1,position.getColumn());
+                options.add(new ChessMove(position,target, ChessPiece.PieceType.BISHOP));
+                options.add(new ChessMove(position,target, ChessPiece.PieceType.ROOK));
+                options.add(new ChessMove(position,target, ChessPiece.PieceType.QUEEN));
+                options.add(new ChessMove(position,target, ChessPiece.PieceType.KNIGHT));
+            }
+        }
         return options;
     }
 
