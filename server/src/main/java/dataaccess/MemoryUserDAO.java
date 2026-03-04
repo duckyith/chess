399773e6f5ab74e199fb82d5dbbea265ce7969b1
duckyith <bradyith@byu.edu>
2026-03-04 15:13:48 -0700

    1 Test and some code quality

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -2,30 +2,29 @@ package dataaccess;
 
 import models.AuthData;
 import models.GameData;
-import models.JoinData;
 import models.UserData;
 
 import java.util.*;
 
 public class MemoryUserDAO implements UserDAO {
 
-    Map<String, UserData> Users = new HashMap<>();
-    Map<String, String> Tokens = new HashMap<>();
-    Map<String, GameData> Games = new HashMap<>();
+    Map<String, UserData> users = new HashMap<>();
+    Map<String, String> tokens = new HashMap<>();
+    Map<String, GameData> games = new HashMap<>();
 
     @Override
     public void register(UserData userData) {
-        Users.put(userData.username(), userData);
+        users.put(userData.username(), userData);
     }
 
     @Override
     public UserData getUser(String username) throws DataAccessException {
-        return Users.get(username);
+        return users.get(username);
     }
 
     @Override
     public void addToken(AuthData authData) {
-        Tokens.put(authData.authToken(), authData.username());
+        tokens.put(authData.authToken(), authData.username());
     }
 
     @Override
@@ -33,26 +32,26 @@ public class MemoryUserDAO implements UserDAO {
         System.out.println("token to be removed");
         System.out.println(token);
         System.out.println("should be here");
-        System.out.println(Tokens.get(token));
-        Tokens.remove(token);
+        System.out.println(tokens.get(token));
+        tokens.remove(token);
         System.out.println("should NOT be here");
-        System.out.println(Tokens.get(token));
+        System.out.println(tokens.get(token));
     }
 
     @Override
     public String getToken(String token) throws DataAccessException {
-        return Tokens.get(token);
+        return tokens.get(token);
     }
 
     @Override
     public void create(GameData game) throws DataAccessException {
-        Games.put(Integer.toString(game.gameID()), game);
+        games.put(Integer.toString(game.gameID()), game);
     }
 
     @Override
     public ArrayList<GameData> list() throws DataAccessException {
         ArrayList<GameData> games = new ArrayList<>();
-        for (GameData game : Games.values()) {
+        for (GameData game : this.games.values()) {
             games.add(new GameData(game.gameID(), game.whiteUsername(), game.blackUsername(), game.gameName(), null));
         }
         return games;
@@ -60,19 +59,19 @@ public class MemoryUserDAO implements UserDAO {
 
     @Override
     public GameData getGame(String gameID) {
-        return Games.get(gameID);
+        return games.get(gameID);
     }
 
     @Override
     public void updateGame(GameData modifiedGame) {
-        Games.replace(Integer.toString(modifiedGame.gameID()), modifiedGame);
+        games.replace(Integer.toString(modifiedGame.gameID()), modifiedGame);
     }
 
     @Override
     public void clear() {
-        Tokens.clear();
-        Users.clear();
-        Games.clear();
+        tokens.clear();
+        users.clear();
+        games.clear();
     }
 
 }
