7ac091d49b0104b292ba66eb21170c35df822daf
duckyith <bradyith@byu.edu>
2026-03-24 21:26:18 -0600

    Finished LoggedOutClient I think

---
+++ client/src/main/java/ui/LoggedInClient.java
@@ -0,0 +1,41 @@
+package ui;
+
+import exception.ResponseException;
+import models.AuthData;
+import models.UserData;
+
+public class LoggedInClient {
+    private final ServerFacade server;
+    public String username;
+    public String authToken;
+    public boolean success = false;
+
+    public LoggedInClient(String serverUrl) {
+        server = new ServerFacade(serverUrl);
+    }
+
+    public String register(String... params) throws ResponseException {
+        if (params.length == 3) {
+            AuthData authData = server.register(new UserData(params[0], params[1], params[2]));
+            if (authData != null && authData.authToken() != null) {
+                username = authData.username();
+                authToken = authData.authToken();
+                success = true;
+                return String.format("User registered. You signed in as %s.", params[0]);
+            }
+        }
+        return "fail";
+    }
+
+    public String login(String... params) throws ResponseException {
+        if (params.length == 2) {
+            AuthData authData = server.login(new UserData(params[0], params[1], null));
+            if (authData != null && authData.authToken() != null) {
+                username = authData.username();
+                authToken = authData.authToken();
+                return String.format("You signed in as %s.", params[0]);
+            }
+        }
+        return "fail";
+    }
+}
